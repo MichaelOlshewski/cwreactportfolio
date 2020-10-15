58beea3b57f8ba9ae309023b061091d7f134c0d5
Michael Olshewski <michael.olshewski@yahoo.com>
2020-10-15 19:28:27 -0400

    Created DB.json and revised the NavBar Component

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaBars } from "react-icons/fa";
+import resume from '../../assets/files/resume.pdf';
 
 function NavBar() {
+
+    function openSlideMenu() {
+        document.querySelector('#side-menu').style.width = "150px"
+    }
+
+    function closeSideMenu() {
+        document.querySelector('#side-menu').style.width = "0px"
+    }
+
     return (
         <header id="header">
-        <div class="headContain">
-            <h1 class="logo">Michael<span class="spanDC"> Olshewski</span></h1>
+        <div className="headContain">
+            <h1 className="logo">Michael<span className="spanDC"> Olshewski</span></h1>
             <nav>
                 <ul>
-                    <li><a href="https://www.linkedin.com/in/michael-olshewski-7b57b714a/" target="_blank"><FaLinkedin/></a></li>
-                    <li><a href="https://www.github.com/MichaelOlshewski/" target="_blank"><FaGithub/></a></li>
-                    <li><a href="assets/files/resume.pdf" target="_blank">Resume</a></li>
+                    <li><a href="https://www.linkedin.com/in/michael-olshewski-7b57b714a/" rel="noopener noreferrer" target="_blank"><FaLinkedin/></a></li>
+                    <li><a href="https://www.github.com/MichaelOlshewski/" target="_blank" rel="noopener noreferrer"><FaGithub/></a></li>
+                    <li><a href={resume} rel="noopener noreferrer" target="_blank">Resume</a></li>
                 </ul>
             </nav>
-            <div class="menu-toggle"><i class="fas fa-bars" onclick="openSlideMenu()" aria-hidden="true"></i></div>
-            <div id="side-menu" class="side-nav">
-                <a href="#" onclick="closeSideMenu()" class="btn-close">&times;</a>
-                <a href="https://www.linkedin.com/in/michael-olshewski-7b57b714a/" target="_blank"><FaLinkedin/></a>
-                <a href="https://www.github.com/MichaelOlshewski/" target="_blank"><FaGithub/></a>
-                <a href="assets/files/resume.pdf" target="_blank">Resume</a>
+            <div className="menu-toggle"><FaBars onClick={openSlideMenu} aria-hidden="true"/></div>
+            <div id="side-menu" className="side-nav">
+                <a href="#nowhere" onClick={closeSideMenu} className="btn-close">&times;</a>
+                <a href="https://www.linkedin.com/in/michael-olshewski-7b57b714a/" rel="noopener noreferrer" target="_blank"><FaLinkedin/></a>
+                <a href="https://www.github.com/MichaelOlshewski/" rel="noopener noreferrer" target="_blank"><FaGithub/></a>
+                <a href={resume} rel="noopener noreferrer" target="_blank">Resume</a>
             </div>
         </div>
     </header>
